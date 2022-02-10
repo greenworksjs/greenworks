fddb01f3cc1863457bd1e9fa8301dd28c2e227e2
Quentin Goinaud <armaldio@gmail.com>
2022-02-10 17:15:31 +0000

    -

--- ci/src/e2e/index.ts
+++ ci/src/e2e/index.ts
@@ -18,7 +18,7 @@ void (async (): Promise<void> => {
     console.log('testing', os, runtime, arch)
 
     if (runtime === 'node') {
-        const { stderr, stdout } = await execa('./node.js', [association[os], arch])
+        const { stderr, stdout } = await execa('node', ['./node.js', association[os], arch])
         console.log('stderr', stderr)
         console.log('stdout', stdout)
     } else {
