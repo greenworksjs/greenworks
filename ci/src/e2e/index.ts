b4f936be321bca03edd8336f1ebcfeadc774e03a
Quentin Goinaud <armaldio@gmail.com>
2022-02-10 17:12:36 +0000

    -

--- ci/src/e2e/index.ts
+++ ci/src/e2e/index.ts
@@ -4,10 +4,6 @@ import mri from 'mri'
 const argv = process.argv.slice(2)
 const args = mri(argv)
 
-// import electron from './electron'
-// import nwjs from './electron'
-import node from './node'
-
 const {
   os, runtime, arch,
 }: Args = args as unknown as Args
